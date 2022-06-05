b11410ec6fac5adb189bd511f41732c47e97cf3e
Juliane Marques <mqs.juliane@gmail.com>
2022-06-05 16:26:29 -0300

    feat: implementing first test idea forms

---
+++ src/__tests__/Formulario.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+test('when input is empty no new participants can be added', () => {
+    render(<Formulario />)
+    // encontrar no DOM  o input - RTL
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+    // encontrar o botao - RTL
+    const btn = screen.getByRole('button');
+    // garantir que o innput esteja no documento - JEST
+    expect(input).toBeInTheDocument();
+    // garantir que o btn esteja desabilitado - JEST
+    expect(button).toBeDisabled();
+});
