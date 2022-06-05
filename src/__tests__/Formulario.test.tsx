9bce4d89b673274830883f9644127580a27f3f5e
Juliane Marques <mqs.juliane@gmail.com>
2022-06-05 16:30:48 -0300

    feat: Form input

--- src/__tests__/Formulario.test.tsx
+++ src/__tests__/Formulario.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import Formulario from '../componentes/Formulario';
 
 test('when input is empty no new participants can be added', () => {
-    render(<Formulario />)
+    render(<Formulario/>)
     // encontrar no DOM  o input - RTL
     const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
     // encontrar o botao - RTL
@@ -10,5 +11,5 @@ test('when input is empty no new participants can be added', () => {
     // garantir que o innput esteja no documento - JEST
     expect(input).toBeInTheDocument();
     // garantir que o btn esteja desabilitado - JEST
-    expect(button).toBeDisabled();
+    expect(btn).toBeDisabled();
 });
