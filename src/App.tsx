763ea39e67fd675f48f23784412474da472392c9
Juliane Marques <mqs.juliane@gmail.com>
2022-06-05 18:12:21 -0300

    feat: css and header

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <head>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"></link>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"></link>
+      </head>
+      
     </div>
   );
 }
--- src/componentes/Formulario.tsx
+++ src/componentes/Formulario.tsx
@@ -1,6 +1,11 @@
+
 const Formulario = () => {
     return (
         <form>
+            <head>
+            <link rel="stylesheet" href="Formulario.css"></link>
+            </head>
+            
             <input type="text"  placeholder="Insira os nomes dos participantes"/>
             <button disabled={true}>Adiconar</button>
         </form>
