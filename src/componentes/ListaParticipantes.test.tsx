6e8683dfbdaf68db3a81a26f690b0fc550d4b73e
Juliane Marques <mqs.juliane@gmail.com>
2022-06-07 14:57:13 -0300

    feat: create listaParticipantes component

--- src/componentes/ListaParticipantes.test.tsx
+++ src/componentes/ListaParticipantes.test.tsx
@@ -1,14 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import ListaParticipantes from './ListaParticipantes'
 
 describe('Uma lista vazia de participantes', () => {
-    render(
-    <RecoilRoot>
-        <ListaParticipantes />
-    </RecoilRoot>
-    )
-
-    const itens = screen.queryAllByRole('listitem');
-    expect(itens).toHaveLength(0)
+    test('deve ser renderizada sem elementos', () => {
+        render(
+            <RecoilRoot>
+                <ListaParticipantes />
+            </RecoilRoot>
+            )
+        
+            const itens = screen.queryAllByRole('listitem');
+            expect(itens).toHaveLength(0)
+    })
+   
 });
---
+++ src/componentes/ListaParticipantes.tsx
@@ -0,0 +1,10 @@
+const ListaParticipantes = () => {
+    const participantes: string[] = [];
+    return (
+        <ul>
+           {participantes.map(participante => <li key={participante}>{participante}</li>)}
+        </ul>
+    )
+};
+
+export default ListaParticipantes
